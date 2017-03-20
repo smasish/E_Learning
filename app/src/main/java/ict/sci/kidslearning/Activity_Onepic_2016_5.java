32be3c4df293d32f607c7d6c4715d451f6c6cd24
smasish <asish_cse@yahoo.com>
2017-03-20 16:21:09 +0600

    16_5 activity flow added

--- app/src/main/java/ict/sci/kidslearning/Activity_Onepic_2016_5.java
+++ app/src/main/java/ict/sci/kidslearning/Activity_Onepic_2016_5.java
@@ -16,8 +16,10 @@ public class Activity_Onepic_2016_5 extends Activity {
     private Context con;
 
     private TextView story;
-    int indexArray=0,question_ind=0;
 
+    private TextView question;
+    int indexArray=0,question_ind=0;
+    int counter =3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,30 +28,43 @@ public class Activity_Onepic_2016_5 extends Activity {
         // getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.one_picture_question);
+        setContentView(R.layout.one_pic_ques16_5);
         con = this;
         //getActionBar().setBackgroundDrawable((getResources().getDrawable(R.drawable.actionbar)));
 
 
-        story = (TextView)findViewById(R.id.des_id);
+      //  story = (TextView)findViewById(R.id.des_id);
 
-        story.setText(R.string.story_2016_5);
+      //  story.setText(R.string.story_2016_5);
 
         question_ind=0;
+
+        indexArray=0;
+
+        counter =3;
+
+        question = (TextView)findViewById(R.id.question_id);
+
+        question.setText(""+getResources().getStringArray(R.array.word_list_2016_6)[question_ind]);
 
     }
 
     public void showbox(View v){
 
-        String instr = ""+getResources().getStringArray(R.array.instruction_set_2016_1)[question_ind];
+        String instr = ""+getResources().getStringArray(R.array.instructions_16_8)[question_ind];
         AlertMessage.showMessage(con,"Instruction",instr);
     }
 
-    public void back(View v){
+    public void next(View v){
 
+        Intent i = new Intent(Activity_Onepic_2016_5.this, WordListActivity2016_5_3.class);
+        overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+        startActivity(i);
+        this.finish();
     }
 
-    public void next(View v){
+    public void back(View v){
+        this.finish();
 
     }
 
